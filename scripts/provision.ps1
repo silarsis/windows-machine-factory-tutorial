a23661d64d61e09d7659fee4c5d729f5debd8c27
Kevin Littlejohn <kevin@littlejohn.id.au>
2015-04-18 01:00:28 +1000

    Update provision.ps1
    for me, lack of the '-y' was causing the script to hang due to waiting for a yes/no/skip dialog

--- scripts/provision.ps1
+++ scripts/provision.ps1
@@ -1,5 +1,5 @@
 (iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')))>$null 2>&1  
-choco install 7zip
+choco install -y 7zip
 choco install seek-dsc
 
 # Disable Windows Updates
